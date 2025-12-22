84087e9a60498716876b218671cae42607368322
Vijay Kollapuri <vijaykollapurii@gmail.com>
2025-12-22 22:54:28 +0530

    Time: 1 ms (97.04%), Space: 42.9 MB (7.92%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,17 +1,24 @@
 class Solution {
     public int reverse(int x) {
-        try{
         if(x == 0) return 0;
-        StringBuilder str = new StringBuilder();
-        if(x < 0) str.append("-");
-        x = Math.abs(x);
+        long rev = 0L;
         while(x != 0){
-            str.append(x % 10);
+            rev = rev * 10 + (x%10);
             x /= 10;
         }
-        return Integer.parseInt(str.toString());
-        } catch (NumberFormatException e){
-            return 0;
-        }
+        return (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) ? 0 :  (int) rev;
+        // try{
+        // if(x == 0) return 0;
+        // StringBuilder str = new StringBuilder();
+        // if(x < 0) str.append("-");
+        // x = Math.abs(x);
+        // while(x != 0){
+        //     str.append(x % 10);
+        //     x /= 10;
+        // }
+        // return Integer.parseInt(str.toString());
+        // } catch (NumberFormatException e){
+        //     return 0;
+        // }
     }
 }
