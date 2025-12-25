a2d7038d829f605c054d6731b88f26371f9dfdeb
Vijay Kollapuri <vijaykollapurii@gmail.com>
2025-12-25 19:34:45 +0530

    Time: 42 ms (70.97%), Space: 112.6 MB (16.67%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.java
@@ -0,0 +1,32 @@
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+        Arrays.sort(happiness);
+        int count = 0; 
+        Long maxHappi = 0L;
+
+        for(int i = happiness.length - 1; i >= 0 && count < k; i--){
+            int cur = happiness[i] - count;
+            if(cur <= 0) break;
+            maxHappi += cur;
+            count++;
+        }
+        return maxHappi;
+    }
+}
+        // Arrays.sort(happiness);
+        // int count = 0, n = happiness.length-1;
+        // long maxHappi = 0L;
+        // while(count < k){
+        //     maxHappi += happiness[n];
+
+        //     int i = n-1;
+        //     while(i != -1){
+        //         if(happiness[i] > 0)
+        //         happiness[i] = happiness[i] - count;
+        //         i--;
+        //     }
+        //     n--;
+        //     count++;
+
+        // }
+        // return maxHappi;
