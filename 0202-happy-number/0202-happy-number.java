55d28d383929925d990aa135b59cf4493fecebc5
Vijay Kollapuri <vijaykollapurii@gmail.com>
2025-12-27 11:00:29 +0530

    Time: 0 ms (100%), Space: 42.4 MB (43.22%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,9 +1,7 @@
 class Solution {
     public boolean isHappy(int n) {
 
-        HashSet<Integer> hs = new HashSet<>();
-
-        while (n != 1){
+        while (n >= 9){
             int num = 0;
             while (n != 0){
                 int r = n % 10;
@@ -11,10 +9,7 @@ class Solution {
                 n = n / 10;
             }
             n = num;
-            if (hs.contains(n))
-                return false;
-            hs.add(n);
         }
-        return n == 1;
+        return n == 1 || n == 7;
     }
 }
