c38347da0f856e60381a6ba45a3c94990b0ccf04
Vijay Kollapuri <vijaykollapurii@gmail.com>
2025-12-28 21:44:03 +0530

    Time: 8 ms (1.4%), Space: 47.3 MB (7.96%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
@@ -1,21 +1,43 @@
+
 class Solution {
     public int countNegatives(int[][] grid) {
-        int count = 0;
-        for(int i=0; i < grid.length; i++){
-            int left = 0, right = grid[i].length - 1;
-            while(left <= right) {
-                int mid = left + (right - left)/2;
-                if(grid[i][mid] < 0) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+        int i = 0;
+        int j = grid[0].length - 1;
+        int ans = 0;
+
+        while (i < grid.length && j >= 0){
+            if (j >= 0 && grid[i][j] < 0){
+                j--;
+                continue;
             }
-            count += grid[i].length - left;
+            else 
+                i++;
+            ans += (grid[0].length - j - 1);
+            System.out.println(i+ " "+j);
         }
-        return count;
+        ans += (grid.length - i) * grid[0].length;
+        return ans ;
     }
 }
+
+// class Solution {
+//     public int countNegatives(int[][] grid) {
+//         int count = 0;
+//         for(int i=0; i < grid.length; i++){
+//             int left = 0, right = grid[i].length - 1;
+//             while(left <= right) {
+//                 int mid = left + (right - left)/2;
+//                 if(grid[i][mid] < 0) {
+//                     right = mid - 1;
+//                 } else {
+//                     left = mid + 1;
+//                 }
+//             }
+//             count += grid[i].length - left;
+//         }
+//         return count;
+//     }
+// }
         // for(int i=0; i<grid.length; i++) {
         //     for(int j=0; j<grid[i].length; j++){
         //         if(grid[i][j] < 0) count++;
