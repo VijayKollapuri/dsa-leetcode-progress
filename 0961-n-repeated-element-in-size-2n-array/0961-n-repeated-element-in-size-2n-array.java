6fc127ffae92d6c8cf73d399283e7984fe0b7d99
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-01-02 10:11:13 +0530

    Time: 21 ms (11.26%), Space: 47.3 MB (75.13%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -5,7 +5,7 @@ class Solution {
 
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i : nums) {
-            map.put(i, map.getOrDefault(i,0) + 1);
+            map.put(i, map.containsKey(i) ? map.get(i)+1 : 1);
             if(map.get(i) == count) return i;
         }
         return -1;
