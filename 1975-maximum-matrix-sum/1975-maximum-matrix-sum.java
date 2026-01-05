17d251166b32f5cae8b508cd6a5d878a178d9030
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-01-05 21:59:20 +0530

    Time: 6 ms (73.94%), Space: 68.1 MB (34.55%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.java
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.java
@@ -1,20 +1,21 @@
 class Solution {
     public long maxMatrixSum(int[][] matrix) {
-        long maxSum = 0, minEle = Integer.MAX_VALUE, count = 0;
+        long res = 0;
+        int minEle = Integer.MAX_VALUE, count = 0;
+
         for(int i=0; i<matrix.length; i++) {
-            for(int j=0; j < matrix[i].length; j++) {
-                maxSum += Math.abs(matrix[i][j]);
-                if(Math.abs(matrix[i][j]) < minEle) {
-                    minEle = Math.abs(matrix[i][j]);
-                }
-                    if(matrix[i][j] < 0) count++;
+            for(int j=0; j<matrix[i].length; j++) {
+                int num = Math.abs(matrix[i][j]);
+                res += num;
+                minEle = Math.min(minEle, num);
+                if(matrix[i][j] < 0) count++;
             }
         }
+
         if(count % 2 == 1) {
-            // maxSum -= (minEle * 2);
-            return maxSum - (minEle * 2);
-        } else {   
-            return maxSum;
+            return res - (minEle * 2);
         }
+
+        return res;
     }
 }
