d671fa8aa2b81141bd5edda0214592b715443b81
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-01-06 21:35:38 +0530

    Time: 0 ms (100%), Space: 42.8 MB (10.38%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,8 +1,8 @@
 class Solution {
     public int addDigits(int num) {
-        // if(num <= 9) return num;
+        if(num <= 9) return num;
         
-        return num == 0 ? 0 : (num % 9 == 0 ? 9 : num % 9);
+        return 1  + (num - 1) % 9;
         
 
     }
