e58bd9b58c4962f5862e21f05993d04dd68ba86f
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-01-06 21:46:32 +0530

    Time: 135 ms (11.96%), Space: 51.2 MB (55.96%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countPrimes(int n) {
+        if(n <= 2) return 0;
+        boolean[] arr = new boolean[n];
+        arr[0] = true; arr[1] = true;
+        for(int i = 2; i < n; i++) {
+            if(!arr[i]) {
+                for(long j = (long) i * i; j < n; j += i) {
+                    arr[(int)j] = true;
+                }
+            }
+        }
+
+        int count = 0;
+        for(boolean a : arr) {
+            if(!a) count++;
+        }
+        return count;
+    }
+}
