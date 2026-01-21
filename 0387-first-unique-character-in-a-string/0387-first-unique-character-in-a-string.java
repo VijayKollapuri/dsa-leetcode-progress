06906db437affcc975e5ba60a0fbfe3d2c8d48ce
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-01-21 15:34:05 +0530

    Time: 35 ms (15.03%), Space: 47.6 MB (9.36%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,24 +1,32 @@
 class Solution {
     public int firstUniqChar(String s) {
-        int[] freq = new int[26];
-        for(char c : s.toCharArray()) {
-            freq[c - 'a']++;
-        }
+        Optional<Character> ch = 
+        s.chars()
+        .mapToObj(c -> (char) c)
+        .filter(c -> s.indexOf(c) == s.lastIndexOf(c))
+        .findFirst();
 
-        for(int i=0; i<s.length(); i++) {
-            char c = s.charAt(i);
-            if(freq[c-'a'] == 1) return i;
-        }
-        // LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+       if(ch.isPresent())
+       return s.indexOf(ch.get());
+        // int[] freq = new int[26];
         // for(char c : s.toCharArray()) {
-        //     map.put(c, map.getOrDefault(c,0)+1);
+        //     freq[c - 'a']++;
         // }
 
-        // for(char c : map.keySet()) {
-        //     if(map.get(c) == 1) {
-        //         return s.indexOf(c);
-        //     }
+        // for(int i=0; i<s.length(); i++) {
+        //     char c = s.charAt(i);
+        //     if(freq[c-'a'] == 1) return i;
         // }
+        // // LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+        // // for(char c : s.toCharArray()) {
+        // //     map.put(c, map.getOrDefault(c,0)+1);
+        // // }
+
+        // // for(char c : map.keySet()) {
+        // //     if(map.get(c) == 1) {
+        // //         return s.indexOf(c);
+        // //     }
+        // // }
    return -1;
     }
 }
