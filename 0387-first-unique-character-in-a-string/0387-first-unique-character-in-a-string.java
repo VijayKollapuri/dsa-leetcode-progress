9c135a9fa51d4a92b0e54a930471c4cf1a34be34
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-01-21 15:34:51 +0530

    Time: 6 ms (84.48%), Space: 46.9 MB (61.08%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,22 +1,23 @@
 class Solution {
     public int firstUniqChar(String s) {
-        Optional<Character> ch = 
-        s.chars()
-        .mapToObj(c -> (char) c)
-        .filter(c -> s.indexOf(c) == s.lastIndexOf(c))
-        .findFirst();
+    //     Optional<Character> ch = 
+    //     s.chars()
+    //     .mapToObj(c -> (char) c)
+    //     .filter(c -> s.indexOf(c) == s.lastIndexOf(c))
+    //     .findFirst();
 
-       if(ch.isPresent())
-       return s.indexOf(ch.get());
-        // int[] freq = new int[26];
-        // for(char c : s.toCharArray()) {
-        //     freq[c - 'a']++;
-        // }
+    //    if(ch.isPresent())
+    //    return s.indexOf(ch.get());
+        int[] freq = new int[26];
+        for(char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
 
-        // for(int i=0; i<s.length(); i++) {
-        //     char c = s.charAt(i);
-        //     if(freq[c-'a'] == 1) return i;
-        // }
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if(freq[c-'a'] == 1) return i;
+        }
+
         // // LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
         // // for(char c : s.toCharArray()) {
         // //     map.put(c, map.getOrDefault(c,0)+1);
