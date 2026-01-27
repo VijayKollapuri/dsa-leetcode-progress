a43798a78cea8f65da363cec434a711c2a324dda
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-01-27 23:12:25 +0530

    Time: 3 ms (99.42%), Space: 46.3 MB (71.88%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,13 +1,13 @@
 class Solution {
     public String intToRoman(int num) {
-        HashMap<Integer, String> map = new HashMap<>();
-        map.put(1,"I");
-        map.put(5,"V");
-        map.put(10,"X");
-        map.put(50,"L");
-        map.put(100,"C");
-        map.put(500,"D");
-        map.put(1000,"M");
+        // HashMap<Integer, String> map = new HashMap<>();
+        // map.put(1,"I");
+        // map.put(5,"V");
+        // map.put(10,"X");
+        // map.put(50,"L");
+        // map.put(100,"C");
+        // map.put(500,"D");
+        // map.put(1000,"M");
 
         String[] str = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         int[] arr = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
