838bf202d4e83d09bf452e2757265f9e6ac9848e
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-02-03 10:02:52 +0530

    Time: 23 ms (9.32%), Space: 57 MB (5.33%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> list = new ArrayList<>();
+    //     HashMap<String, List<String>> map = new HashMap<>();
+    //     for(String s : strs){
+    //         char ch[] = s.toCharArray();
+    //         Arrays.sort(ch);
+    //         String s1 = new String(ch);
+    //         map.putIfAbsent(s1, new ArrayList<String>());
+    //         map.get(s1).add(s);
+    //     }
+
+    //    for(List<String> str : map.values()) list.add(str);
+       
+    //    return list;
+
+    Map<String,List<String>> map = Arrays.stream(strs)
+    .collect(Collectors.groupingBy( s -> {
+        // char ch[] = s.toCharArray();
+        // Arrays.sort(ch);
+        // return new String(ch);
+        int[] count = new int[26];
+        for (char c : s.toCharArray()) {
+        count[c - 'a']++;
+        }
+return Arrays.toString(count);}));
+
+    list.addAll(map.values());
+    return list;
+
+    }
+}
