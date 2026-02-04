16b35aa01ab6128124c49954896c82ee0be05831
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-02-04 18:21:04 +0530

    Time: 1 ms (82.19%), Space: 42.8 MB (37.44%) - LeetHub

---
+++ 0984-string-without-aaa-or-bbb/0984-string-without-aaa-or-bbb.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String strWithout3a3b(int a, int b) {
+        if(a >= b) {
+           return make(a, b, 'a', 'b');
+        } else {
+           return make(b, a, 'b', 'a');
+        }
+    }
+
+    public static String make(int a, int b, char c, char d) {
+        StringBuilder res = new StringBuilder();
+        int div = a/2;
+        while(a > 0 || b > 0) {
+            if(a > 2 && a >= b) {
+                res.append(c);
+                res.append(c);
+                a -= 2;
+            } else if(a!=0) {
+                res.append(c);
+                a -= 1;
+            }
+            
+             if(b != 1 && b > div) {
+                res.append(d);
+                res.append(d);
+                b -= 2;
+            } else if(b!=0) {
+                res.append(d);
+                b -= 1;
+            }
+        }
+        return res.toString();
+    }
+}
