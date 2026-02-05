7db20a2c3a5b68431abe9138bc9b46f7f71f15b2
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-02-05 22:51:47 +0530

    Time: 0 ms (100%), Space: 42.5 MB (13.48%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        int ar[] = new int [46];
+        ar[0] = 0;
+        ar[1] = 1;
+        ar[2] = 2;
+
+        for (int i=3;i<=45;i++){
+            ar[i] = ar[i-1] + ar[i-2];
+        }
+       return ar[n];
+    }
+
+    
+}
