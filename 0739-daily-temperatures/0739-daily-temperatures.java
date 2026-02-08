f4e3286ad0a6b8bdbb22c24c69af77a0906decff
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-02-08 19:43:38 +0530

    Time: 22 ms (94.85%), Space: 103.9 MB (80.13%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] dailyTemperatures(int[] arr) {
+        int n = arr.length;
+        ArrayDeque<Integer> q = new ArrayDeque<>();
+        q.offer(0);
+        int[] res = new int[n];
+        for(int i = 1; i < n; i++) {
+            while(!q.isEmpty() && arr[i] > arr[q.peek()]) {
+                int idx = q.pop();
+                res[idx] = i - idx;
+            }
+            q.push(i);
+        }
+        return res;
+    }
+}
