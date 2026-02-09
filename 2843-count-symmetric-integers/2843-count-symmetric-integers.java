e1d6ac35f7e77994cab003695876e61323af0cef
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-02-09 17:34:09 +0530

    Time: 32 ms (55.88%), Space: 46.7 MB (14.62%) - LeetHub

--- 2843-count-symmetric-integers/2843-count-symmetric-integers.java
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.java
@@ -1,28 +1,51 @@
+
 class Solution {
     public int countSymmetricIntegers(int low, int high) {
         int count = 0;
         for(int i = low; i <= high; i++) {
             String s = String.valueOf(i);
-            if(s.length() > 1 && ((s.length() % 2) == 0)) {
-                int n = s.length()/2;
-                String s1 = s.substring(0, n);
-                String s2 = s.substring(n, (n+n));
-                if(sum(s1) == sum(s2)){
-                     count++;
-                System.out.println(s);
+            int n = s.length();
+            if((n == 1) || (n % 2) == 1) continue;
+
+            int r = 0, l = 0;
+            int mid = n / 2;
+            for(int j = 0; j < mid; j++) {
+                l += s.charAt(j) - '0';
             }
+
+            for(int j = mid; j < n; j++) {
+                r += s.charAt(j) - '0';
             }
+
+            if(l == r) count++;
         }
         return count;
     }
-
-    public static int sum(String s) {
-        int n = Integer.parseInt(s);
-        int res = 0;
-        while(n > 0) {
-            res += n % 10;
-            n /= 10;
-        }
-        return res;
-    }
 }
+
+//         int count = 0;
+//         for(int i = low; i <= high; i++) {
+//             String s = String.valueOf(i);
+//             if(s.length() > 1 && ((s.length() % 2) == 0)) {
+//                 int n = s.length()/2;
+//                 String s1 = s.substring(0, n);
+//                 String s2 = s.substring(n, (n+n));
+//                 if(sum(s1) == sum(s2)){
+//                      count++;
+//                 System.out.println(s);
+//             }
+//             }
+//         }
+//         return count;
+//     }
+
+//     public static int sum(String s) {
+//         int n = Integer.parseInt(s);
+//         int res = 0;
+//         while(n > 0) {
+//             res += n % 10;
+//             n /= 10;
+//         }
+//         return res;
+//     }
+// }
