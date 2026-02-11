0fd2ce371922dd3239d89c5f9593493878b32a4e
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-02-11 17:33:04 +0530

    Time: 2 ms (90.03%), Space: 48.1 MB (16.73%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,18 +1,27 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        for(int i : nums) list.add(i);
-          list =  list.stream()
-		           .collect(Collectors.collectingAndThen(
-				   Collectors.partitioningBy(i -> i == 0), map -> {
-					   List<Integer> li = new ArrayList<>();
-					   li.addAll(map.get(false));
-					   li.addAll(map.get(true));
-					   return li;
-				   }));
-
-        for(int i = 0; i < list.size(); i++) {
-            nums[i] = list.get(i);
+        int i = 0; 
+        for(int j=0; j < nums.length; j++) {
+            if(nums[j] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
         }
+        // List<Integer> list = new ArrayList<>();
+        // for(int i : nums) list.add(i);
+        //   list =  list.stream()
+		//            .collect(Collectors.collectingAndThen(
+		// 		   Collectors.partitioningBy(i -> i == 0), map -> {
+		// 			   List<Integer> li = new ArrayList<>();
+		// 			   li.addAll(map.get(false));
+		// 			   li.addAll(map.get(true));
+		// 			   return li;
+		// 		   }));
+
+        // for(int i = 0; i < list.size(); i++) {
+        //     nums[i] = list.get(i);
+        // }
     }
 }
