efa71634a3417a8cb6a724ca003c164c7c423e87
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-02-22 12:56:54 +0530

    Time: 9 ms (27.6%), Space: 49.2 MB (63.07%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,14 +1,23 @@
 class Solution {
     public int[] replaceElements(int[] arr) {
-        
-        for(int i = 0; i < arr.length; i++) {
-            int max = 0; 
-            for(int j = i + 1; j < arr.length; j++) {
-                max = (arr[j] > max) ? arr[j] : max;
-            }
-            arr[i] = max;
-        }
-        arr[arr.length - 1] = -1;
-        return arr;
+        int[] a = {-1};
+        IntStream.iterate(arr.length - 1, i -> i >= 0, i -> i - 1)
+                 .forEach(i -> {
+                    int temp = arr[i];
+                    arr[i] = a[0];
+                    a[0] = Math.max(a[0], temp); 
+                 });
+                 return arr;
     }
 }
+    //     int n = arr.length;
+    //    IntStream.range(0, n-1)
+    //             .forEach(i -> {
+    //                  arr[i] =  IntStream.range(i + 1, n)
+    //                           .map(j -> arr[j])
+    //                           .max()
+    //                           .getAsInt();
+    //    });
+
+    //     arr[n - 1] = -1;
+    //     return arr;
