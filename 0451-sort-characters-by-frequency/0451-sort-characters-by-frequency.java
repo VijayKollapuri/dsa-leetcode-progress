c2dc8f2288e931160f31c0c229c4ed5afc7bde1c
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-02-25 21:42:21 +0530

    Time: 15 ms (49.8%), Space: 46.5 MB (89.24%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,22 +1,26 @@
 class Solution {
     public String frequencySort(String s) {
-        return 
-        Stream.of(s.split(""))
-              .collect(Collectors.groupingBy(i->i, Collectors.counting()))
-              .entrySet()
-              .stream()
-              .sorted(Map.Entry.<String, Long>comparingByValue(Comparator.reverseOrder())
-                        .thenComparing(Map.Entry.comparingByKey()))    
-              .map(e -> e.getKey().repeat(e.getValue().intValue()))
-              .collect(Collectors.joining());
+        // return 
+        // Stream.of(s.split(""))
+        //       .collect(Collectors.groupingBy(i->i, Collectors.counting()))
+        //       .entrySet()
+        //       .stream()
+        //       .sorted(Map.Entry.<String, Long>comparingByValue(Comparator.reverseOrder())
+        //                 .thenComparing(Map.Entry.comparingByKey()))    
+        //       .map(e -> e.getKey().repeat(e.getValue().intValue()))
+        //       .collect(Collectors.joining());
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c : s.toCharArray()) map.put(c, map.getOrDefault(c, 0) + 1);
+        PriorityQueue<Map.Entry<Character, Integer>> q = new PriorityQueue<>((a, b) -> Integer.compare(b.getValue(), a.getValue()));
+        q.addAll(map.entrySet());
+        StringBuilder str = new StringBuilder();
+        while(!q.isEmpty()) {
+            Map.Entry<Character, Integer> m = q.poll();
+            str.append(String.valueOf(m.getKey()).repeat(m.getValue()));
+        }
+        return str.toString();
+
 
     }
 }
-
-/*
-flatMap(e -> LongStream.range(0, e.getValue())
-                                     .takeWhile(i -> i < e.getValue())
-                                     .mapToObj(i -> e.getKey())
-                                     .collect(Collectors.joining())
-                                     ).reduce("", (a, b) -> a + b);
-*/
