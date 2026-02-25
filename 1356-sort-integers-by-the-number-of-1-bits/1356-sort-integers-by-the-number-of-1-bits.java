a5d930c0f60bbffdb3711504cb7a4c3f59e46d05
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-02-25 11:02:37 +0530

    Time: 6 ms (93.7%), Space: 46.6 MB (70.12%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -1,10 +1,24 @@
 class Solution {
     public int[] sortByBits(int[] arr) {
-       return Arrays.stream(arr)
-                    .boxed()
-                    .sorted(Comparator.comparingInt(Integer::bitCount).thenComparing(i -> i))
-                    .mapToInt(Integer::intValue)
-                    .toArray();
+        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> {
+            if(a[0] != b[0]) return Integer.compare(a[0], b[0]);
+            else return Integer.compare(a[1], b[1]);
+        });
+
+        for(int i : arr) {
+            q.offer(new int[]{Integer.bitCount(i), i});
+        }
+
+       int i = 0;
+       while(!q.isEmpty()) {
+        arr[i++] = q.poll()[1];
+       }
+       return arr;
+    //    return Arrays.stream(arr)
+    //                 .boxed()
+    //                 .sorted(Comparator.comparingInt(Integer::bitCount).thenComparing(i -> i))
+    //                 .mapToInt(Integer::intValue)
+    //                 .toArray();
         
     }
 }
