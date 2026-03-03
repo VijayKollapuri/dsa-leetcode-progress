4ea71495aeee08ef87053f4647e8b1b613234d7a
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-03-03 19:12:48 +0530

    Time: 2 ms (96.13%), Space: 48.7 MB (10.48%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,26 +1,17 @@
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<numbers.length; i++) {
-            map.put(numbers[i], i + 1);
+    public int[] twoSum(int[] arr, int target) {
+        int i  = 0, j = arr.length - 1;
+        while(i < j) {
+           int sum = arr[i] + arr[j];
+           if(sum == target) {
+            return new int[]{i + 1, j + 1};
+           } else if(sum > target) {
+            j--;
+           } else {
+            i++;
+           }
         }
 
-        for(int i=0; i<numbers.length; i++){
-            int complement = target - numbers[i];
-            if(map.containsKey(complement)) {
-                return new int[]{i+1, map.get(complement)};
-            }
-        }
-        return new int[]{-1,-1};
+        return new int[]{-1, -1};
     }
 }
-
-
-        // int left = 0, right = numbers.length-1;
-        // while(left < right) {
-        //     int sum = numbers[left] + numbers[right];
-        //     if(sum == target) {
-        //         return new int[]{left+1, right+1};
-        //     } else if(sum > target) right--;
-        //     else left++;
-        // }
