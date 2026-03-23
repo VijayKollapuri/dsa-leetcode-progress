93372f2603dcef514d00b7769cb63204baa9dc9d
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-03-23 21:59:38 +0530

    Time: 0 ms (100%), Space: 42 MB (67.5%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,20 +1,20 @@
 class Solution {
     public int climbStairs(int n) {
+        // if(n == 0) return 0;
+        // if(n == 1) return 1;
+        // return fibo(n);
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
 
-        if (n == 1 || n == 2){
-            return n;
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        int ar[] = new int [46];
-        
-        int x = 1;
-        int y = 2;
-        int res = 0;
-
-        for (int i=3;i<=n;i++){
-            res = x + y;
-            x = y;
-            y = res;
-        }
-       return res;
+        return dp[n];
     } 
+
+    private static int fibo(int n) {
+        if(n == 0 || n == 1) return 1;
+        return fibo(n - 1) + fibo(n - 2);
+    }
 }
