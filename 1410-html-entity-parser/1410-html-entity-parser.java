152dfdc1d84b3a1303afeb7b1d35acc4543106ea
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-03-24 23:55:03 +0530

    Time: 20 ms (95.31%), Space: 47.7 MB (64.84%) - LeetHub

--- 1410-html-entity-parser/1410-html-entity-parser.java
+++ 1410-html-entity-parser/1410-html-entity-parser.java
@@ -1,40 +1,48 @@
 class Solution {
 
-    private static HashMap<String, Character> map = new HashMap<>();
-    static {
-        map.put("&quot;", '"');
-        map.put("&amp;", '&');
-        map.put("&gt;", '>');
-        map.put("&lt;", '<');
-        map.put("&frasl;", '/');
-    }
-    private static String s2 = "&apos;";
-    private static String c2 = "'";
+    // private static HashMap<String, Character> map = new HashMap<>();
+    // static {
+    //     map.put("&quot;", '"');
+    //     map.put("&amp;", '&');
+    //     map.put("&gt;", '>');
+    //     map.put("&lt;", '<');
+    //     map.put("&frasl;", '/');
+    // }
+    // private static String s2 = "&apos;";
+    // private static String c2 = "'";
 
     public String entityParser(String text) {
-        StringBuilder str = new StringBuilder(text);
-        int i = 0;
-        while(i < str.length()) {
-            if(str.charAt(i) == '&') {
-                StringBuilder temp = new StringBuilder("&");
-                int j = i + 1; 
-                    while(j < str.length() && str.charAt(j) != ';') { 
-                        temp.append(str.charAt(j));
-                    j++;
-                }
+        // StringBuilder str = new StringBuilder(text);
+        // int i = 0;
+        // while(i < str.length()) {
+        //     if(str.charAt(i) == '&') {
+        //         StringBuilder temp = new StringBuilder("&");
+        //         int j = i + 1; 
+        //             while(j < str.length() && str.charAt(j) != ';') { 
+        //                 temp.append(str.charAt(j));
+        //             j++;
+        //         }
                 
-                temp.append(";");
+        //         temp.append(";");
 
-                if(temp.toString().equals(s2)) str.replace(i, j+1, c2);
-                else {
-                    if(map.containsKey(temp.toString())){
-                    str.replace(i, j + 1, map.get(temp.toString()) + "");
-                }
-                }
-            }
-            i++;
-        }
+        //         if(temp.toString().equals(s2)) str.replace(i, j+1, c2);
+        //         else {
+        //             if(map.containsKey(temp.toString())){
+        //             str.replace(i, j + 1, map.get(temp.toString()) + "");
+        //         }
+        //         }
+        //     }
+        //     i++;
+        // }
 
-        return str.toString();
+        // return str.toString();
+
+        return 
+            text.replace("&quot;", '"'+"")
+                .replace("&apos;", "'")
+                .replace("&gt;", ">")
+                .replace("&lt;", "<")
+                .replace("&frasl;", "/")
+                .replace("&amp;", "&");
     }
 }
