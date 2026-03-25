ac9fc865fdc6e776ea52e5011c9dabbf540da736
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-03-25 16:34:03 +0530

    Time: 81 ms (5.02%), Space: 91 MB (77.93%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -5,21 +5,35 @@ class Solution {
 
        for(int i : nums) set.add(i);
 
-       ArrayList<Integer> list = new ArrayList<>(set);
-       int count = 1;
-       int temp = 1;
-       int prev = list.get(0);
-       for(int i = 1; i < list.size(); i++) {
-            if(list.get(i) - prev == 1) {
-                temp++;
-                prev = list.get(i);
-            } else {
-                prev = list.get(i);
-                temp = 1;
-            }
+    //    ArrayList<Integer> list = new ArrayList<>(set);
+    //    int prev = list.get(0);
+    //    for(int i = 1; i < list.size(); i++) {
+    //         if(list.get(i) - prev == 1) {
+    //             temp++;
+    //             prev = list.get(i);
+    //         } else {
+    //             prev = list.get(i);
+    //             temp = 1;
+    //         }
 
-            count = Math.max(temp, count);
-       }
+    //         count = Math.max(temp, count);
+    //    }
+
+    int count = 0, prev = 0, temp = 0;
+    for(int i : set) {
+        if(i - prev == 1) {
+            temp++;
+            prev = i;
+        } else {
+            prev = i;
+            temp = 1;
+        }
+
+        count = Math.max(count, temp);
+    }
+
+
+
         return count;
 
     }
