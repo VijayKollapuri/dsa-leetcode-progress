aaf8282ba9b77d6dedff2c6f9047ba9063882563
Vijay Kollapuri <vijaykollapurii@gmail.com>
2026-06-22 21:21:21 +0530

    Time: 5 ms (79.32%), Space: 45.7 MB (86.64%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0) return false;
+        int num = 0, temp = x;
+        while(x > 0) {
+           num = (num * 10) + (x % 10);
+            x /= 10;
+        }
+        return num == temp;
+    }
+}
+
